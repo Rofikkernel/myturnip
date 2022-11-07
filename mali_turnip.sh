c1795ffa47e8d7ec591ebfc1cf08413c9945e11e
RofikKernel <rsoft3d@gmail.com>
2022-11-07 16:47:52 +0700

    Update mali_turnip.sh

--- mali_turnip.sh
+++ mali_turnip.sh
@@ -80,7 +80,7 @@ meson build-android-aarch64 --cross-file $workdir/mesa-main/android-aarch64 -Dbu
 
 
 echo "Compiling build files ..." $'\n'
-ninja -C build-android-aarch64 &> $workdir/ninja_log
+ninja -C build-android-aarch64
 
 
 
